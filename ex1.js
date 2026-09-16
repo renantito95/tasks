cfe3b8aa0bdb896f6fa0b28c5a521d6802bf6e98
renan <renan95tito@gmail.com>
2026-09-16 20:51:00 -0300

    function async

---
+++ ex1.js
@@ -0,0 +1,11 @@
+const URL = 'https://dummyjson.com/products';
+
+async function chamarApi() {
+    const resp = await fetch(URL);
+    if (resp.status === 200) {
+        const obj = await resp.json();
+        console.log(obj);
+    }
+}
+
+chamarApi();
